256928b818437106c72908f7ede576c70e499727
DESKTOP-OVFV7BQ\SK <bhagwan@Shopkirana.com>
2024-02-12 15:34:17 +0530

    chnages in otp screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,5 +4,6 @@ const kPrimaryColor = Color(0xFF0196CE);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
 const textFiledBackgroundColour = Color(0xffEFFAFF);
 const gryColor = Color(0xff858585);
+const blackSmall = Color(0xff1E1E1E);
 
 const double defaultPadding = 16.0;
--- lib/view/otp_screens/OtpScreen.dart
+++ lib/view/otp_screens/OtpScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pinput/pinput.dart';
 
 class OtpScreen extends StatelessWidget {
   const OtpScreen({super.key});
@@ -12,24 +13,49 @@ class OtpScreen extends StatelessWidget {
         child: Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 30, top: 50),
-                  child: SvgPicture.asset("assets/icons/scale_up_logo.svg"),
-                ),
-              ],
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 30, top: 50),
-              child: Text(
-                "Enter\nVerification Code",
-                style: TextStyle(fontWeight: FontWeight.normal, fontSize: 40),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 30,top: 50,right: 30,bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(height: 69,width: 51,
+                  alignment:Alignment.topLeft,
+                  child:  Image.asset('assets/images/scale.png')
               ),
-            ),
-          ],
+              const SizedBox(height: 50,),
+
+              const Text(
+                'Enter \nVerification Code',
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 40, color: Colors.black),
+              ),
+              const SizedBox(height: 30,),
+              const Text(
+                'We just sent to +91 XXXX XXX224',
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 15, color: Colors.black),
+              ),
+              const SizedBox(height: 45,),
+
+              Pinput(
+                // Without Validator
+                // If true error state will be applied no matter what validator returns
+                forceErrorState: true,
+                // Text will be displayed under the Pinput
+                errorText: 'Error',
+
+                /// ------------
+                /// With Validator
+                /// Auto validate after user tap on keyboard done button, or completes Pinput
+                pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+                validator: (pin) {
+                  if (pin == '2224') return null;
+                  return 'Pin is incorrect';
+                },
+              ),
+
+            ],
+          ),
         ),
       ),
     ));
