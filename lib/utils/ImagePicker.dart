a633d6c65027aa419dc2aed0ab18e52c943c52fa
AtulSinghSk <atul@shopkirana.com>
2024-02-13 19:30:21 +0530

    ImagePicker

--- lib/utils/ImagePicker.dart
+++ lib/utils/ImagePicker.dart
@@ -1,9 +1,13 @@
+
 import 'package:flutter/material.dart';
+
 import 'package:flutter_svg/svg.dart';
 
-class ImagePickerWidgets extends StatelessWidget {
 
-  const ImagePickerWidgets({Key? key}) : super(key: key);
+
+
+class ImagePickerWidgets extends StatelessWidget {
+  const ImagePickerWidgets({Key? key,}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,27 +19,49 @@ class ImagePickerWidgets extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Container(
-              child:Column(
-                children: [
-                  SvgPicture.asset('assets/icons/camera_icon.svg'),
-                  SizedBox(height: 10),
-                  Text("Camera",style: TextStyle(color: Colors.black),)
-                ],
-              ) ,
+
+              child: GestureDetector(
+                onTap: () {
+                  print("click Camera");
+
+                },
+
+                child: Column(
+                  children: [
+                    SvgPicture.asset(
+                      'assets/icons/photo_camera_icon.svg', width: 50,
+                      height: 50,),
+                    SizedBox(height: 10),
+                    Text("Camera", style: TextStyle(color: Colors.black),)
+                  ],
+                ),
+              ),
+
             ),
+
+
             Container(
-              child:Column(
-                children: [
-                  SvgPicture.asset('assets/images/gallery.svg'),
-                  SizedBox(height: 10),
-                  Text("Gallery",style: TextStyle(color: Colors.black),)
-                ],
-              ) ,
+              child: GestureDetector(
+                onTap: () {
+                  print("Click Gallery");
+
+
+                },
+
+                child: Column(
+                  children: [
+                    SvgPicture.asset(
+                      'assets/icons/photo_gallery_icon.svg', width: 50,
+                      height: 50,),
+                    SizedBox(height: 10),
+                    Text("Gallery", style: TextStyle(color: Colors.black),)
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
   }
-
 }
